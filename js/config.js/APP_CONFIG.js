a2e684a9302f1f922110a074fc26209b661c20ae
David Josue <dj.morenogonzalez@ugto.mx>
2026-09-19 14:07:06 -0600

    create: creamos los archivos de audius, config y states.ui con su contenido

---
+++ js/config.js/APP_CONFIG.js
@@ -0,0 +1,14 @@
+export const APP_CONFIG = {
+    audius: {
+        apiKey : 'c0194dcde360b596726a5a7311b4215e1e34b83d'
+    },
+    search: {
+        minLennth: 2, 
+        limits: 20
+    },
+    trending: {
+        limit: 12,
+        time: 'week'
+    }
+
+}
